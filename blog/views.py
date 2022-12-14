e0461444012457f0899b08796f134498f4bf8784
LauraMayock <mcfadden.laura@yahoo.com>
2022-12-14 13:07:19 +0000

    fix like tab

--- blog/views.py
+++ blog/views.py
@@ -231,4 +231,5 @@ class PostLike(View):
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
-            
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
